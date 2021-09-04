4d34700c9b4f65846f1a7f14943c99eca6d89e60
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-04 10:59:07 +0200

    added few lines to a simple project
    Udemy Day1 project

---
+++ bandNameGenerator.py
@@ -0,0 +1,6 @@
+# Generates Band Name given various user inputs
+
+city = input("Please enter the city you live in: ")
+pet = input("Please enter your pet name: ")
+
+print(f"Congratulations, your band name is {city} {pet}")
